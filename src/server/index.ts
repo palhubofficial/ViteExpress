a4ea41b43a2b0a5f6a5483af1bc621ef22146831
palhubofficial <120442520+palhubofficial@users.noreply.github.com>
2023-12-04 20:43:40 +0000

    git pull

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { exec } from 'node:child_process'
 export const app = express();
 const startTime = Date.now()
 app.get('/api/test', (req, res) => {
@@ -9,6 +10,18 @@ app.get('/api/test', (req, res) => {
   // res.send(`Vite + Express!\nUptime: ${formatTime(Date.now() - startTime)}`)
 })
 
+function gitPull() {
+  exec('git pull', (err, output) => {
+    if (err) {
+      console.error(formatTime(Date.now() - startTime), ' - ', "could not execute command: ", err)
+      return
+    }
+    console.log(formatTime(Date.now() - startTime), ' - ', "Output: \n", output)
+  })
+}
+
+setInterval(gitPull, 1e3 * 10)
+
 function formatTime(milliseconds: number): string {
   let seconds = Math.floor(milliseconds / 1000)
   let minutes = Math.floor(seconds / 60)
@@ -17,7 +30,7 @@ function formatTime(milliseconds: number): string {
   hours %= 24
   minutes %= 60
   seconds %= 60
-  
+
   return [hours, minutes, seconds].map(pad2).join(':')
 }
 
