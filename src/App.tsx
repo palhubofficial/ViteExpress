ad5d2b459615ba1d11af6391c4ea2dd881d946bc
palhubofficial <120442520+palhubofficial@users.noreply.github.com>
2023-12-05 01:52:15 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <main>
       <h1>Vite + Express</h1>
-      <p>Hello</p>
+      <p>Hello EXPRESS and VITE</p>
       React ⚛️ + Vite ⚡ + Replit 🌀
     </main>
   )
